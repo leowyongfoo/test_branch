ec2c0835179098e568c0571fcf10dadbd62d7a79
leowyongfoo <jacksonleow6@gmail.com>
2023-06-02 14:33:08 +0800

    test commit try to push to master

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter App in master branch',
+      title: 'Flutter App in master branch test push',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
