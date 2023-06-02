aec59b25e2924b56b50cc21c2093a2bdcf41888f
leowyongfoo <jacksonleow6@gmail.com>
2023-06-02 14:22:46 +0800

    test commit in backup branch

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter App in master branch',
+      title: 'Flutter App in backup branch',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
