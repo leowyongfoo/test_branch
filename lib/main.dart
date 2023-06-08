6241162e7412969c1d6072f0d1e0a186f8f4e6fa
leowyongfoo <jacksonleow6@gmail.com>
2023-06-08 12:54:15 +0800

    test rebase merge

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   //benson testing branch
+  //test rebase merge
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
