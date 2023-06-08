070bd9f4ebb194302d4c584bc6de0d2d0e0054d4
leowyongfoo <61189340+leowyongfoo@users.noreply.github.com>
2023-06-08 12:01:37 +0800

    new feature by added Test1, push second to master (#22)

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   //test1 feature new branch
   //test2 new feature added
   //test 2 push to master first
+  //test1 add new feature but push second before update from master
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
