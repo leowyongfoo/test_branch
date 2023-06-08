e856d052afe42766de21b684db4786421d85252e
leowyongfoo <jacksonleow6@gmail.com>
2023-06-08 12:35:20 +0800

    new feature by added Test2

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   //benson testing branch
-  //test1 feature new branch
-  //test2 new feature added
-  //test 2 push to master first
-  //test1 add new feature but push second before update from master
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
