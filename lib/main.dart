bd538e8044a9bc755ee05fdcd86cb1214bc39620
leowyongfoo <jacksonleow6@gmail.com>
2023-06-09 12:43:27 +0800

    feature 2 push

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   //benson testing branch
-  //feature 1 changes
+  //feature 2 changes
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
