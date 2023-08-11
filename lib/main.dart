894c2ee81699325ab089366ab7dbcb336cc2e833
leowyongfoo <jacksonleow6@gmail.com>
2023-08-11 15:55:04 +0800

    test stash commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter App MAKE CHANGES',
+      title: 'New lexus app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
